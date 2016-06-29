aba9fdb10985cf15ddeaa3b0b740998ea5ea6d58
takahashisan <appler21op.mwp@docomo.ne.jp>
2016-06-29 03:52:00 +0000

    ユーザー設定画面(edit.html.erb)の編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+# 　validates_uniqueness_of :name
+#   validates_presence_of :name
+
   has_many :posts
   has_many :favorites, dependent: :destroy
 
